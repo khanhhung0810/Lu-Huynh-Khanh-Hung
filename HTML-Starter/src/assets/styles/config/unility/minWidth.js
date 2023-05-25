09e4bf3aee7f022e85da1be2d8eeac713df2acea
khanhhung0810 <lhkhungg@gmail.com>
2023-05-25 10:50:04 +0700

    new

---
+++ HTML-Starter/src/assets/styles/config/unility/minWidth.js
@@ -0,0 +1,16 @@
+const minWidth = {
+  0: '0',
+  full: '100%',
+  min: 'min-content',
+  max: 'max-content',
+  200: '200px',
+  500: '500px',
+  450 :'450px',
+  600: '600px',
+  700: '700px',
+  800: '800px',
+  900: '900px'
+}
+module.exports = {
+  minWidth
+}
