70b656f30243372f811f2dcec255838e2a53d10c
khanhhung0810 <lhkhungg@gmail.com>
2023-05-29 14:45:02 +0700

    new

--- HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
+++ HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
@@ -9,13 +9,13 @@ export default class SlickSlider {
       }
     }
     addSlick (){
-      this.$this.find('.slider').slick({
+      this.$this.find('.sliders').slick({
         'slidesToShow': 3,
         arrows: true,
-        prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>left-arrow</title><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="none" stroke="#565656"><polyline points="23,30 9,16 23,2 " transform="translate(0, 0)"></polyline></g></svg></button>',
-        nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>right-arrow</title><g stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="none" stroke="#565656"><polyline points="9,2 23,16 9,30 " transform="translate(0, 0)"></polyline></g></svg></button>',
+        prevArrow: '<button class="slicks-prev arrows h1 text-blue-400"><span class="icomoon icon-chevron-left"></span><span class="sr-only">Prev slider</span></button>',
+        nextArrow: '<button class="slicks-next arrows h1 text-blue-400"><span class="icomoon icon-chevron-right"></span><span class="sr-only">Next slider</span></button>',
         dots: true,
-      
+        dotsClass:'slicks-dotss',
         responsive: [
             {
             breakpoint: 767,
