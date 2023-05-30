c83897438884e9d66b7fc584137235d7ba456623
khanhhung0810 <lhkhungg@gmail.com>
2023-05-30 16:05:45 +0700

    new

--- HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
+++ HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
@@ -11,6 +11,7 @@ export default class SlickSlider {
     addSlick (){
       this.$this.find('.sliders').slick({
         'slidesToShow': 3,
+        variableWidth:true,
         arrows: true,
         prevArrow: '<button class="slicks-prev arrows h1 text-blue-400"><span class="icomoon icon-chevron-left"></span><span class="sr-only">Prev slider</span></button>',
         nextArrow: '<button class="slicks-next arrows h1 text-blue-400"><span class="icomoon icon-chevron-right"></span><span class="sr-only">Next slider</span></button>',
