ac39088e7e77899caa771fef17c9a15fe6403de8
khanhhung0810 <lhkhungg@gmail.com>
2023-05-31 10:01:14 +0700

    new

--- HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
+++ HTML-Starter/src/assets/javascripts/modules/SlickSlider.js
@@ -16,7 +16,7 @@ export default class SlickSlider {
         prevArrow: '<button class="slicks-prev arrows h1 text-blue-400"><span class="icomoon icon-chevron-left"></span><span class="sr-only">Prev slider</span></button>',
         nextArrow: '<button class="slicks-next arrows h1 text-blue-400"><span class="icomoon icon-chevron-right"></span><span class="sr-only">Next slider</span></button>',
         dots: true,
-        dotsClass:'slicks-dotss',
+        dotsClass:'slick-dotss',
         responsive: [
             {
             breakpoint: 767,
